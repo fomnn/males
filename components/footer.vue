2db2b9caaa6009604d1d17c9234ac629d0e13e8e
fomnn <95164774+fomnn@users.noreply.github.com>
2025-01-27 14:35:44 +0700

    update

--- components/footer.vue
+++ components/footer.vue
@@ -4,9 +4,9 @@
 
 <template>
   <div class="flex justify-center items-center gap-24 px-[2rem] md:px-[6rem] border h-[4rem] w-full backdrop-blur-sm bg-opacity-10 z-20 bg-white">
-    <div class="flex items-center h-full gap-3">
+    <div class="flex gap-3 items-center h-full">
       <img src="/logo.svg" alt="" class="h-1/3">
-      <p>by <span class="text-primary font-semibold">Oman</span></p>
+      <p>by <span class="font-semibold text-primary">Oman</span></p>
     </div>
   </div>
 </template>

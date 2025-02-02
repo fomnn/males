f470d44ead451a82766a4cb02f6133a88c3cc809
fomnn <95164774+fomnn@users.noreply.github.com>
2025-02-02 16:58:04 +0700

    only run middleware on client

--- middleware/superadmin-auth.global.ts
+++ middleware/superadmin-auth.global.ts
@@ -1,6 +1,10 @@
 export default defineNuxtRouteMiddleware(async (to, _from) => {
   const { loggedIn } = useUserSession();
 
+  if (import.meta.server) {
+    return;
+  }
+
   if (to.path.startsWith("/super") && !to.path.startsWith("/super/auth")) {
     if (!loggedIn.value) {
       return "/super/auth/login";

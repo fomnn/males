93b940d1591fc486628112a3de70a991c0e9d3a5
fomnn <95164774+fomnn@users.noreply.github.com>
2025-02-02 17:36:12 +0700

    update

--- middleware/superadmin-auth.global.ts
+++ middleware/superadmin-auth.global.ts
@@ -1,17 +1,17 @@
 export default defineNuxtRouteMiddleware(async (to, _from) => {
   const { loggedIn } = useUserSession();
 
-  if (import.meta.server) {
-    return;
-  }
+  // if (import.meta.server) {
+  //   return;
+  // }
 
   if (to.path.startsWith("/super") && !to.path.startsWith("/super/auth")) {
     if (!loggedIn.value) {
-      return "/super/auth/login";
+      return navigateTo("/super/auth/login", { redirectCode: 301 });
     }
   }
 
   if (to.path === "/super/auth/login" && loggedIn.value) {
-    return "/super";
+    return navigateTo("/super", { redirectCode: 301 });
   }
 });
